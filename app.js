5d63f7aa9536ce022324e92d5d992acf2a70ba59
Tasha <xtasherx@gmail.com>
2020-08-10 22:58:33 -0500

    Laid out basic html structure and added psuedocode

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // add handles for index.html
+// need to access highscores when link in nav is pressed
 // start button
-// countdown
+// ------needs to hide .start-div
+// ------needs to display .questions-div
+// ------needs to start countdown
 // generate a new question each time an answer is chosen
-// log correct/incorrect to the page
+// ------replace question textContent and button textContent
+// ------track correct/incorrect answer in a a variable and log it to the page
 // after last question create input to add initals to high score
 // display high score
