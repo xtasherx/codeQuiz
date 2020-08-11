381a368aac6a9c28dce12aa27ea4b0c1418fc567
Tasha <xtasherx@gmail.com>
2020-08-10 22:25:52 -0500

    Created basic project files and logged out psuedocode

---
+++ app.js
@@ -0,0 +1,7 @@
+// add handles for index.html
+// start button
+// countdown
+// generate a new question each time an answer is chosen
+// log correct/incorrect to the page
+// after last question create input to add initals to high score
+// display high score
