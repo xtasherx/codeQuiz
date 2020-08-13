cbc46f5353ef2fe9bfb8935562370c08c1b42868
Tasha <xtasherx@gmail.com>
2020-08-12 23:38:38 -0500

    Added timer function and additional div for timeout

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// add handles for index.html
+// DOM selection
 const highScoreButton = document.querySelector(".navbar-brand");
 const goBackButton = document.querySelector(".go-back");
 const clearScoreButton = document.querySelector(".clear-score");
@@ -7,25 +7,28 @@ const startDiv = document.querySelector(".start-div");
 const questDiv = document.querySelector(".question-div");
 const initDiv = document.querySelector(".initials-div");
 const scoreDiv = document.querySelector(".highscore-div");
+const timeOut = document.querySelector(".timeout-div");
 const multChoice = document.querySelector(".mult-choice");
 const scoreList = document.querySelector(".scoreList");
 const scoreLis = document.querySelector(".score");
 const corrIncor = document.querySelector(".right-wrong");
 const quest = document.querySelector(".question");
 const ansBtn = document.querySelectorAll(".ans-btn");
+const tryBtn = document.querySelector(".try-again");
 const initButton = document.querySelector(".init-btn");
 const initInput = document.querySelector(".init-input");
 const displayScore = document.querySelector(".display-score");
+const timeLeft = document.querySelector(".time-left");
 
 const qAndA = {
   questions: [
-    "What is your name?",
+    "What method is used to convert a string into a number?",
     "What is your quest?",
     "What is your favorite color?",
     "What is the air-speed velocity of an Unladen Swallow",
     "This isn't even a question I just need something.",
   ],
-  answers0: ["Tasha", "To find the holy grail.", "Green", "placeholder"],
+  answers0: [".parseInt()", ".length()", ".toUpperCase", ".toString()"],
   answers1: ["your mom", "To find the holy grail.", "Green", "placeholder"],
   answers2: [
     "your moms mom",
@@ -39,18 +42,20 @@ const qAndA = {
   timer: 0,
   counter: 0,
   questCount: 0,
+  secondsLeft: 60,
 };
 /////////////////////////////////////////////
 /////////////functions//////////////////////
 ////////////////////////////////////////////
 
 // function to show/hide divs
-function hideDivs(div, div1, div2, div3) {
+function hideDivs(div, div1, div2, div3, div4) {
   div.style.display = "block";
   if (div.style.display === "block") {
     div1.style.display = "none";
     div2.style.display = "none";
     div3.style.display = "none";
+    div4.style.display = "none";
   }
 }
 // pull answer lists from qAndA and display them as button text for each question
@@ -64,38 +69,81 @@ function answerGen(count, answers) {
     qAndA.counter = 0;
   }
 }
+// displays correct/incorrect answer and tracks score
+function goodAnswer(answer) {
+  if (event.target.textContent === answer) {
+    corrIncor.textContent = `Correct`;
+    qAndA.score++;
+  } else {
+    corrIncor.textContent = `Incorrect`;
+    qAndA.secondsLeft = qAndA.secondsLeft - 10;
+  }
+}
+// countdown
+function setTime() {
+  var timerInterval = setInterval(function () {
+    qAndA.secondsLeft--;
+    timeLeft.textContent = `Time Left ${qAndA.secondsLeft}`;
+
+    if (qAndA.secondsLeft <= 0) {
+      hideDivs(timeOut, startDiv, initDiv, questDiv, scoreDiv);
+      qAndA.secondsLeft = 60;
+      timeLeft.textContent = "";
+      qAndA.questCount = 0;
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
 /////////////////////////////////////////////////////////
 ////////////////Event Listeners//////////////////////////
 /////////////////////////////////////////////////////////
 
-// need to access highscores when link in nav is pressed
+//access highscores when link in nav is pressed
 highScoreButton.addEventListener("click", () => {
-  hideDivs(scoreDiv, questDiv, initDiv, startDiv);
+  hideDivs(scoreDiv, questDiv, initDiv, startDiv, timeOut);
   scoreList.style.display = "block";
 });
+
 // go back to main page from highscore
 goBackButton.addEventListener("click", () => {
-  hideDivs(startDiv, questDiv, initDiv, scoreDiv);
+  hideDivs(startDiv, questDiv, initDiv, scoreDiv, timeOut);
 });
+
+// go back to main page from timeout
+tryBtn.addEventListener("click", () => {
+  hideDivs(startDiv, questDiv, initDiv, scoreDiv, timeOut);
+});
+
 // start button
 startBtn.addEventListener("click", () => {
-  hideDivs(questDiv, startDiv, initDiv, startDiv);
+  hideDivs(questDiv, startDiv, initDiv, startDiv, timeOut);
   // prep first question and add 1 to questCount
   quest.innerHTML = `${qAndA.questions[qAndA.questCount]}`;
   answerGen(0, qAndA.answers0);
+  qAndA.secondsLeft = 60;
+  setTime();
   qAndA.questCount++;
 });
+
 // button to clear high scores
 clearScoreButton.addEventListener("click", () => {
   scoreList.textContent = "";
-  // also needs to clear from localStorage
 });
 
 multChoice.addEventListener("click", (event) => {
-  if ((event.target.matches = "button")) {
-    console.log(event.target.textContent);
-    console.log(qAndA.counter);
+  // check for correct answers
+  if (qAndA.questCount === 1) {
+    goodAnswer(qAndA.answers0[0]);
+  } else if (qAndA.questCount === 2) {
+    goodAnswer(qAndA.answers1[0]);
+  } else if (qAndA.questCount === 3) {
+    goodAnswer(qAndA.answers2[0]);
+  } else if (qAndA.questCount === 4) {
+    goodAnswer(qAndA.answers3[0]);
+  } else if (qAndA.questCount === 5) {
+    goodAnswer(qAndA.answers4[0]);
   }
+
   // continue to populate questions as answer buttons are chosen
   quest.innerHTML = `${qAndA.questions[qAndA.questCount]}`;
   // generate the correct answer choices for each question
@@ -103,21 +151,12 @@ multChoice.addEventListener("click", (event) => {
   answerGen(2, qAndA.answers2);
   answerGen(3, qAndA.answers3);
   answerGen(4, qAndA.answers4);
-  if (condition) {
-  }
 
   qAndA.questCount++;
 
-  if (event.target.textContent === "Answer 1") {
-    corrIncor.textContent = `Correct`;
-    qAndA.score++;
-  } else {
-    corrIncor.textContent = `Incorrect`;
-    // subtract time from timer
-  }
   // all questions have been answered move to initials div
   if (qAndA.questCount > 5) {
-    hideDivs(initDiv, startDiv, questDiv, scoreDiv);
+    hideDivs(initDiv, startDiv, questDiv, scoreDiv, timeOut);
     displayScore.textContent = `Your final score is: ${qAndA.score}`;
     qAndA.score = 0;
     qAndA.questCount = 0;
@@ -129,11 +168,7 @@ initButton.addEventListener("click", () => {
   let newScoreSpot = document.createElement("li");
   newScoreSpot.textContent = initInput.value;
   scoreList.appendChild(newScoreSpot);
-  hideDivs(scoreDiv, questDiv, initDiv, startDiv);
+  hideDivs(scoreDiv, questDiv, initDiv, startDiv, timeOut);
   scoreList.style.display = "block";
   // needs to store the score to rank high scores
 });
-// ------needs to start countdown and display it in nav
-// when a wrong answer is entered subtract time
-// generate a new question each time an answer is chosen
-// ------replace questiols -on should clear scoreList Ul
